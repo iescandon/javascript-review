6dab3b35570dbc790ab265ae70025cb2006b1740
Inez Escandon <inezescandon@Inezs-MacBook-Air.local>
2021-03-14 16:59:00 -0500

    more more changes

--- assets/logic.js
+++ assets/logic.js
@@ -8,16 +8,22 @@ var cardEl = document.getElementById("card");
 var i = 0;
 
 function createCard() {
-imageEl.setAttribute("src", employees[i].picture)
-nameEl.textContent = employees[i].name.first + " " + employees[i].name.last;
-ageEl.textContent = employees[i].age;
-emailEl.textContent = employees[i].email;
-phoneEl.textContent = employees[i].phone;
-if (employees[i].gender === "female") {
-    cardEl.setAttribute("style", "width: 18rem; background-color: pink;")
-} else {
-    cardEl.setAttribute("style", "width: 18rem; background-color: lightblue;")
-}
+    imageEl.setAttribute("src", employees[i].picture)
+    nameEl.textContent = employees[i].name.first + " " + employees[i].name.last;
+    ageEl.textContent = employees[i].age;
+    emailEl.textContent = employees[i].email;
+    phoneEl.textContent = employees[i].phone;
+    if (employees[i].gender === "female") {
+        // cardEl.setAttribute("style", "width: 18rem; background-color: pink;")
+        // cardEl.setAttribute("class", "card girl") //replaces the entire class with what is in the second parameter
+        cardEl.classList.remove("boy") //need to remove before you add the new one
+        cardEl.classList.add("girl")
+    } else {
+        // cardEl.setAttribute("style", "width: 18rem; background-color: lightblue;")
+        // cardEl.setAttribute("class", "card boy")
+        cardEl.classList.remove("girl")
+        cardEl.classList.add("boy")
+    }
 }
 
 function goNextCard() {
